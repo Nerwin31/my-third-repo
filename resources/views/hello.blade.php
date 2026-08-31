77e5b8c9d7f7345e8d6d59dbcfff21af79d79b4e
laxamana john nerwin <laxamanajohnnerwin_act@plmun.edu.ph>
2026-08-31 15:44:36 +0800

    Add Bootstrap CSS and JS

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello Laravel</title>
+
+    <title>My First Laravel App</title>
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 </head>
 <body>
-    <h1>My First Laravel App</h1>
+
+    <div class="container mt-5">
+        <h1 class="text-primary">Bootstrap Successfully Integrated!</h1>
+
+        <p>
+            This Laravel application is now using local Bootstrap files.
+        </p>
+
+        <button class="btn btn-primary">
+            Test Button
+        </button>
+    </div>
+
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    //welcome.blade.php
-    //hello.blade.php
-    return view('hello');
+    return view('welcome');
 });
